801d4290928c0ee67b7feb8897b043579a955231
ALIAKBERAAKASH <ali.akber@brainstation-23.com>
2022-07-05 22:57:33 +0600

    feat: list all folders and files

--- main.go
+++ main.go
@@ -12,4 +12,20 @@ func main() {
 		log.Println(err)
 	}
 	fmt.Println(path)
+
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	files, err := f.Readdir(0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, v := range files {
+		fmt.Println(v.Name(), v.IsDir())
+	}
+
 }
