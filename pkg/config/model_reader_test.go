3d27b52a1137b90097b7ba50c5225fdb1c502917
Ali Akber <ali852609@gmail.com>
2022-12-23 17:20:37 +0600

    add: test for ReadJsonConfig

--- pkg/config/model_reader_test.go
+++ pkg/config/model_reader_test.go
@@ -9,7 +9,59 @@ func TestReadJsonConfig(t *testing.T) {
 
 	fileName := "/Users/bs274/go_projects/file_tree/config.json"
 
-	log.Println("Trying to read: " + fileName)
+	var modelReader = GetModelReader()
+
+	jsonString, err := modelReader.ReadJsonConfig(fileName)
+
+	if err != nil {
+		log.Println(err.Error())
+		t.Error("Failed to read config file")
+	} else {
+		log.Println(string(jsonString))
+	}
+
+}
+
+func TestConvertModel(t *testing.T) {
+
+	var jsonString = []byte(`{
+        "name": "Feature",  
+        "type": "FD",
+        "children": [
+			{
+				"name": "Feature",  
+				"type": "FD",
+				"children": []
+			},
+			{
+				"name": "Feature",  
+				"type": "FD",
+				"children": [
+					{
+						"name": "Feature",  
+						"type": "FD",
+						"children": []
+					}
+				]
+			}
+		]
+    }`)
+
+	var modelReader = GetModelReader()
+
+	output, err := modelReader.ConvertModel(jsonString)
+
+	if err != nil {
+		log.Println(err.Error())
+		t.Error("Failed to convert model")
+	} else {
+		log.Println(output)
+	}
+
+}
+
+func TestModelReaderComplete(t *testing.T) {
+	fileName := "/Users/bs274/go_projects/file_tree/config.json"
 
 	var modelReader = GetModelReader()
 
@@ -22,4 +74,12 @@ func TestReadJsonConfig(t *testing.T) {
 		log.Println(string(jsonString))
 	}
 
+	output, err := modelReader.ConvertModel(jsonString)
+
+	if err != nil {
+		log.Println(err.Error())
+		t.Error("Failed to convert model")
+	} else {
+		log.Println(output)
+	}
 }
